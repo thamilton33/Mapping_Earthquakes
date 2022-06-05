040f01b7f6f788a2ead1376dce8123a05ace0494
Trevor Hamilton <trevorhamilton3@gmail.com>
2022-06-05 09:04:00 -0500

    13.4.1 upload

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,8 +1,20 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map("mapid").setView([40.7, -94.5], 4); 
+// Create the map object with a center and zoom leve.
+let map = L.map("mapid").setView([34.0522, -118.2437], 4); 
+///13.4.1 module single point on a map
+L.circle([34.0522, -118.2437], {
+   radius: 100
+}).addTo(map);
+
+L.circleMarker([34.0522, -118.2437],{
+   radius: 100,
+    color: "black",
+  fillColor: '#ffffa1'
+}).addTo(map);
+
+
 
 // We create the tile layer that will be the background of our map.
   //let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {    
